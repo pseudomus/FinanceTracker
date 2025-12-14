f7004798e0ee2edab5b5ee0973ccfbf8268f3268
Luca Lacerda <lucagabriel520@gmail.com>
2025-12-13 22:31:20 -0300

    feat: project added

---
+++ FinanceTracker/FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Luca Lacerda on 13/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinanceTracker/FinanceTrackerTests/FinanceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerTests.swift
+//  FinanceTrackerTests
+//
+//  Created by Luca Lacerda on 13/12/25.
+//
+
+import Testing
+@testable import FinanceTracker
+
+struct FinanceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
